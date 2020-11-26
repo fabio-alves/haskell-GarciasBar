c5d1dd8b58e1158cf72f82c201756f60bb5aed42
Fabioa Alves <fabioalves.dev@gmail.com>
2020-11-26 17:47:49 -0300

    add POST and GET

--- src/Handler/Bebida.hs
+++ src/Handler/Bebida.hs
@@ -1,15 +1,47 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Handler.Bebida where
 
 import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 
+formBebida :: Form Bebida
+formBebida = renderBootstrap3 BootstrapBasicForm $ Bebida
+  <$> areq textField "Nome: " Nothing
+  <*> areq textField "Comentario: " Nothing
+  <*> areq doubleField "Valor: " Nothing
+
 getBebidaR :: Handler Html
-getBebidaR = undefined
+getBebidaR = do 
+  (formWidget, _) <- generateFormPost formBebida
+  defaultLayout $ do
+    [whamlet|
+        <form action=@{BebidaR} method=post>
+          ^{formWidget}
+          <input type="submit" value="Enviar">
+    |]
 
 postBebidaR :: Handler Html
-postBebidaR = undefined 
+postBebidaR = do 
+    ((result, _), _) <- runFormPost formBebida
+    case result of
+      FormSuccess bebida -> do  
+        pid <- runDB $ insert bebida
+        redirect (DescR pid) 
+      _ -> redirect HomeR
+
+getDescR :: BebidaId -> Handler Html
+getDescR pid = do
+    bebida <- runDB $ get404 pid 
+    defaultLayout $ do
+      [whamlet|
+        <ul>
+          <li> Nome: #{bebidaNome bebida}
+          <li> Comentario: #{bebidaComentario bebida}
+          <li> Valor: #{bebidaValor bebida}
+    |]
