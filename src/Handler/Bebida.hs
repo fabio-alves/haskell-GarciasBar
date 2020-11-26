232e7565dd108874bc275f42061de08992c862b3
Fabioa Alves <fabioalves.dev@gmail.com>
2020-11-26 13:37:44 -0300

    limpeza framework

---
+++ src/Handler/Bebida.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Bebida where
+
+import Import
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
+
+getBebidaR :: Handler Html
+getBebidaR = undefined
+
+postBebidaR :: Handler Html
+postBebidaR = undefined 
